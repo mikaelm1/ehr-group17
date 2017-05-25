34fac591585d3f78670c50a95e1dde3ee5bad1b0
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2017-05-25 10:41:03 -0700

    Added unit test scaffolding

---
+++ test/base.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('Array', function(){
+    describe('#indexof', function(){
+        it('should return -1 when the value is not present', function(){
+            assert.equal(-1, [1,2,3].indexOf(4));
+        });
+    });
+});
